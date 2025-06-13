fa2079d63012c2462f2eefbe341e5512cfb5c84f
dungtran48 <beosdora48@gmail.com>
2025-06-13 20:17:10 +0700

    Hoàn thành chức năng review

--- controllers/client/review.controller.js
+++ controllers/client/review.controller.js
@@ -30,3 +30,56 @@ module.exports.review = async (req, res) => {
     });
   }
 };
+module.exports.updateReview = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const newData = { ...req.body, updatedAt: new Date() };
+    console.log(newData);
+
+    const result = await Review.updateOne(
+      { _id: id },
+      newData
+    );
+
+    console.log(result);
+
+    if (!result) {
+      return res.status(404).json({
+        message: 'Không tìm thấy đánh giá hoặc không có thay đổi nào.'
+      });
+    }
+
+    res.status(200).json({
+      message: 'Cập nhật đánh giá thành công',
+      data: result
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: 'Lỗi khi cập nhật đánh giá',
+      error: error.message
+    });
+  }
+};
+module.exports.deleteReview = async (req, res) => {
+  const id = req.params.id;
+    try {
+      const deletedProduct = await Review.deleteOne({
+        _id: id
+      });
+  
+      if (!deletedProduct) {
+        return res.status(404).json({ message: 'Không tìm thấy đánh giá để xóa' });
+      }
+  
+      res.status(200).json({
+        message: 'Xóa đánh giá thành công',
+        product: deletedProduct
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Lỗi khi xóa đánh giá',
+        error: error.message
+      });
+    }
+};
--- routes/client/review.route.js
+++ routes/client/review.route.js
@@ -5,5 +5,7 @@ const controller = require("../../controllers/client/review.controller");
 
 router.post("/", controller.review);
 
+router.patch("/:id", controller.updateReview);
 
+router.delete("/:id", controller.deleteReview);
 module.exports = router
